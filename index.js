8f4a47c55838003d1dc70de12496022be880f984
ParrishJ <jarparrish@gmail.com>
2019-06-05 15:58:47 -0700

    mvp plus parts of stretch

--- index.js
+++ index.js
@@ -35,6 +35,82 @@ server.get('/api/cohorts', (req, res) => {
         });
 });
 
+server.get('/students', (req, res) => {
+    db('students')
+        .then(students => {
+            res.status(200).json(students)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Could not retrieve students" })
+        });
+});
+
+server.get('/api/cohorts/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .first()
+        .then(cohort => {
+            if (cohort) {
+                res.status(200).json(cohort)
+            } else {
+                res.status(404).json({ error: "No record for this cohort" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+});
+
+server.get('/api/cohorts/:id/students', (req, res) => {
+    const id = req.params.id
+    studentsByCohort(id)
+        .then(students => {
+            res.status(200).json(students)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+server.put('/api/cohorts/:id', (req, res) => {
+    const changes = req.body
+    db('cohorts')
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ message: "Cohort not found" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.delete('/api/cohorts/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                const unit = count > 1 ? 'cohorts' : 'cohort'
+                res.status(200).json({ message: `${count} ${unit} deleted` })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+function studentsByCohort(id) {
+    return db('students')
+        .join('cohorts', 'cohorts.id', 'students.cohort_id')
+        .select('students.id', 'students.name as studentname', 'cohorts.name as cohort')
+        .where('students.cohort_id', id)
+}
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
