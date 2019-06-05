0cdefe3b7db9e338f66f8d45d5af127f84151720
ParrishJ <jarparrish@gmail.com>
2019-06-05 14:55:26 -0700

    seeds added and ran, first two crud ops implemented

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.post('/api/cohorts', (req, res) => {
+    if (req.body.name) {
+        db('cohorts')
+            .insert(req.body, 'id')
+            .then(ids => {
+                res.status(201).json(ids)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "Could not add cohort" })
+            })
+    } else {
+        res.status(400).json({ error: "Must incluse cohort name" })
+    }
+})
+
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Could not retrieve cohorts" })
+        });
+});
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
