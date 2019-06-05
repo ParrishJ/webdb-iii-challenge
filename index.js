7441ab3356219248ee290d9d361cbc7f08338acc
ParrishJ <jarparrish@gmail.com>
2019-06-05 14:09:55 -0700

    Tables successfully migrated, added to db

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+    console.log(`***SERVER UP ON PORT ${port}`)
+)
